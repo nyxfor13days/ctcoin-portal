aa41014895c14ebebbaa21ccc500b9af665fd43f
nyxfor13days <harsh1739371@gmail.com>
2022-06-07 15:39:35 +0530

    minor changes in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,17 +18,31 @@ import {
 	AccountBalanceWalletRounded,
 	LogoutRounded,
 	CellTowerRounded,
+	MenuRounded,
+	DoubleArrowRounded,
+	CompareArrowsRounded,
+	ShoppingCartRounded,
+	CollectionsRounded,
 } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-	const [anchorEl, setAnchorEl] = useState(null);
-	const open = Boolean(anchorEl);
-	const handleClick = (event) => {
-		setAnchorEl(event.currentTarget);
+	const [navigationAnchorEl, setNavigationAnchorEl] = useState(null);
+	const openNavigationMenu = Boolean(navigationAnchorEl);
+	const handleNavigationClick = (event) => {
+		setNavigationAnchorEl(event.currentTarget);
 	};
-	const handleClose = () => {
-		setAnchorEl(null);
+	const handleNavigationClose = () => {
+		setNavigationAnchorEl(null);
+	};
+
+	const [accountAnchorEl, setAccountAnchorEl] = useState(null);
+	const openAccountMenu = Boolean(accountAnchorEl);
+	const handleAccountClick = (event) => {
+		setAccountAnchorEl(event.currentTarget);
+	};
+	const handleAccountClose = () => {
+		setAccountAnchorEl(null);
 	};
 
 	return (
@@ -39,10 +53,84 @@ const Header = () => {
 					direction='row'
 					justifyContent='space-between'
 					alignItems='center'>
+					<Grid item xs={3}>
+						<IconButton
+							id='navigation-button'
+							aria-controls={
+								openNavigationMenu
+									? 'navigation-menu'
+									: undefined
+							}
+							aria-haspopup='true'
+							aria-expanded={
+								openNavigationMenu ? 'true' : undefined
+							}
+							onClick={handleNavigationClick}>
+							<MenuRounded color='primary' />
+						</IconButton>
+
+						<Menu
+							id='navigation-menu'
+							anchorEl={navigationAnchorEl}
+							keepMounted
+							open={openNavigationMenu}
+							onClose={handleNavigationClose}>
+							<MenuItem onClick={handleNavigationClose}>
+								<Link to='/transfer'>
+									<Stack
+										direction='row'
+										alignItems='center'
+										sx={{ minWidth: '200px' }}>
+										<ListItemText>Transfer</ListItemText>
+										<DoubleArrowRounded color='primary' />
+									</Stack>
+								</Link>
+							</MenuItem>
+
+							<MenuItem onClick={handleNavigationClose}>
+								<Link to='/convert'>
+									<Stack
+										direction='row'
+										alignItems='center'
+										sx={{ minWidth: '200px' }}>
+										<ListItemText>Convert</ListItemText>
+										<CompareArrowsRounded color='primary' />
+									</Stack>
+								</Link>
+							</MenuItem>
+
+							<MenuItem onClick={handleNavigationClose}>
+								<Link to='/buy'>
+									<Stack
+										direction='row'
+										alignItems='center'
+										sx={{ minWidth: '200px' }}>
+										<ListItemText>Buy</ListItemText>
+										<ShoppingCartRounded color='primary' />
+									</Stack>
+								</Link>
+							</MenuItem>
+
+							<MenuItem onClick={handleNavigationClose}>
+								<Link to='/nft-marketplace'>
+									<Stack
+										direction='row'
+										alignItems='center'
+										sx={{ minWidth: '200px' }}>
+										<ListItemText>
+											NFT Marketplace
+										</ListItemText>
+										<CollectionsRounded color='primary' />
+									</Stack>
+								</Link>
+							</MenuItem>
+						</Menu>
+					</Grid>
+
 					<Grid item xs={6}>
 						<Stack
 							direction='row'
-							justifyContent='flex-start'
+							justifyContent='center'
 							alignItems='center'
 							spacing={2}>
 							<Logo
@@ -52,36 +140,10 @@ const Header = () => {
 									'/assets/images/logo.png'
 								}
 							/>
-
-							<Stack direction='row' spacing={1}>
-								<Link to='/dashboard'>
-									<Typography variant='caption'>
-										Transfer
-									</Typography>
-								</Link>
-
-								<Link to='/dashboard'>
-									<Typography variant='caption'>
-										Convert
-									</Typography>
-								</Link>
-
-								<Link to='/dashboard'>
-									<Typography variant='caption'>
-										Buy
-									</Typography>
-								</Link>
-
-								<Link to='/dashboard'>
-									<Typography variant='caption'>
-										NFT Marketplace
-									</Typography>
-								</Link>
-							</Stack>
 						</Stack>
 					</Grid>
 
-					<Grid item xs={6}>
+					<Grid item xs={3}>
 						<Stack
 							direction='row'
 							justifyContent='flex-end'
@@ -89,23 +151,23 @@ const Header = () => {
 							spacing={0.5}>
 							<IconButton
 								id='menu-button'
-								aria-controls={open ? 'basic-menu' : undefined}
+								aria-controls={
+									openAccountMenu ? 'account-menu' : undefined
+								}
 								aria-haspopup='true'
-								aria-expanded={open ? 'true' : undefined}
-								onClick={handleClick}>
+								aria-expanded={
+									openAccountMenu ? 'true' : undefined
+								}
+								onClick={handleAccountClick}>
 								<AccountCircleRounded color='primary' />
 							</IconButton>
 
-							<Typography variant='caption'>
-								0x93403...8B4
-							</Typography>
-
 							<Menu
-								id='basic-menu'
-								anchorEl={anchorEl}
-								open={open}
-								onClose={handleClose}>
-								<MenuItem onClick={handleClose}>
+								id='account-menu'
+								anchorEl={accountAnchorEl}
+								open={openAccountMenu}
+								onClose={handleAccountClose}>
+								<MenuItem onClick={handleAccountClose}>
 									<ListItemIcon>
 										<AccountBalanceWalletRounded
 											color='primary'
@@ -120,7 +182,7 @@ const Header = () => {
 									</Typography>
 								</MenuItem>
 
-								<MenuItem onClick={handleClose}>
+								<MenuItem onClick={handleAccountClose}>
 									<ListItemIcon>
 										<CellTowerRounded
 											color='primary'
@@ -137,7 +199,7 @@ const Header = () => {
 
 								<Divider />
 
-								<MenuItem onClick={handleClose}>
+								<MenuItem onClick={handleAccountClose}>
 									<ListItemIcon>
 										<LogoutRounded
 											color='warning'
